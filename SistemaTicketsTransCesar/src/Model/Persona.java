74b6a8d7cf86d7544be40ec7b3c14a9f1c5038e1
Felix Herazo <fdherazo@unicesar.edu.co>
2026-03-08 00:11:13 -0500

    feat: agregar constructor y encapsulamiento a clase Persona

--- SistemaTicketsTransCesar/src/Model/Persona.java
+++ SistemaTicketsTransCesar/src/Model/Persona.java
@@ -7,6 +7,30 @@
  *
  * @author COMPUMAX
  */
-public class Persona {
+public abstract class Persona implements Imprimible{
+    protected String nombre;
+    protected String cedula;
+
+    public Persona(String nombre, String cedula) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    
     
 }

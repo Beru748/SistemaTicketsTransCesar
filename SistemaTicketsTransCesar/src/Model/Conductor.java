e52a656948883dd328f55b8e06847004aedae83d
Felix Herazo <fdherazo@unicesar.edu.co>
2026-03-08 00:19:04 -0500

    feat: agregar constructor y encapsulamiento a clase Conductor

--- SistemaTicketsTransCesar/src/Model/Conductor.java
+++ SistemaTicketsTransCesar/src/Model/Conductor.java
@@ -1,3 +1,6 @@
+
+import modelo.Persona;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +10,36 @@
  *
  * @author COMPUMAX
  */
-public class Conductor {
+public class Conductor extends Persona {
+    private String numeroLicencia;
+    private String categoriaLicencia;
+
+    public Conductor(String numeroLicencia, String categoriaLicencia) {
+        this.numeroLicencia = numeroLicencia;
+        this.categoriaLicencia = categoriaLicencia;
+    }
+
+    public Conductor(String numeroLicencia, String categoriaLicencia, String id, String nombre, String apellido) {
+        super(id, nombre, apellido);
+        this.numeroLicencia = numeroLicencia;
+        this.categoriaLicencia = categoriaLicencia;
+    }
+
+    public String getNumeroLicencia() {
+        return numeroLicencia;
+    }
+
+    public void setNumeroLicencia(String numeroLicencia) {
+        this.numeroLicencia = numeroLicencia;
+    }
+
+    public String getCategoriaLicencia() {
+        return categoriaLicencia;
+    }
+
+    public void setCategoriaLicencia(String categoriaLicencia) {
+        this.categoriaLicencia = categoriaLicencia;
+    }
+    
     
 }

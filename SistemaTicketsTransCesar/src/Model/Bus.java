192d34db97e58a0ca9e3edf82394aabc117b9837
Beru748 <ericksamirwaton@gmail.com>
2026-03-12 10:29:15 -0500

    fix: erreglar algunos error con las implementaciones y la herencia

--- SistemaTicketsTransCesar/src/Model/Bus.java
+++ SistemaTicketsTransCesar/src/Model/Bus.java
@@ -1,20 +1,15 @@
 package Model;
 
-public class Bus extends vehiculo{
-
-    public Bus(String placa, String modelo, int capacidadMaxima, int pasajerosActuales, double precioBaseTicket) {
-        super(placa, modelo, 80, pasajerosActuales, 10000);
-    }
+public class Bus extends Vehiculo{
     
+    public Bus(String placa, String modelo, int capacidadMaxima, int pasajerosActuales, double precioBaseTicket,
+            String tipoVehiculo, boolean estado, String idConductor, String origen, String destino) {
+        super(placa, modelo, 80, pasajerosActuales, 10000, "BUS", estado, idConductor,
+                origen, destino);
+    }
+
     @Override
     public void imprimirDetalle(){
         System.out.println("Bus: " + this.toString());
-    }
-
-    @Override
-    public void calcularTotal(){
-        double total = getPrecioBaseTicket() * getPasajerosActuales();
-
-        System.out.println("Total recaudado del Bus: $" + total);
     }
 }
--- SistemaTicketsTransCesar/src/Model/Microbus.java
+++ SistemaTicketsTransCesar/src/Model/Microbus.java
@@ -1,19 +1,15 @@
 package Model;
 
-public class Microbus extends vehiculo{
-    public Microbus(String placa, String modelo, int capacidadMaxima, int pasajerosActuales, double precioBaseTicket) {
-        super(placa, modelo, 12, pasajerosActuales, 2400);
-    }
+public class Microbus extends Vehiculo{
     
+    public Microbus(String placa, String modelo, int capacidadMaxima, int pasajerosActuales, double precioBaseTicket,
+            String tipoVehiculo, boolean estado, String idConductor, String origen, String destino) {
+        super(placa, modelo, 12, pasajerosActuales, 2400, "MICROBUS", estado, idConductor,
+                origen, destino);
+    }
+
     @Override
     public void imprimirDetalle(){
         System.out.println("Microbus: " + this.toString());
-    }
-
-    @Override
-    public void calcularTotal(){
-        double total = getPrecioBaseTicket() * getPasajerosActuales();
-
-        System.out.println("Total recaudado del Microbus: $" + total);
     }
 }

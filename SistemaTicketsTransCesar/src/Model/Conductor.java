422582a17d63f3b8c40842616d253cacda1a9f4e
Felix Herazo <fdherazo@unicesar.edu.co>
2026-03-12 22:01:18 -0500

    feat(model): crear clase Conductor con sus atributos

--- SistemaTicketsTransCesar/src/Model/Conductor.java
+++ SistemaTicketsTransCesar/src/Model/Conductor.java
@@ -1,54 +1,11 @@
 
-import modelo.Persona;
+package Model;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author COMPUMAX
- */
 public class Conductor extends Persona {
+
     private String numeroLicencia;
     private String categoriaLicencia;
 
-    public Conductor(String numeroLicencia, String categoriaLicencia) {
-        this.numeroLicencia = numeroLicencia;
-        this.categoriaLicencia = categoriaLicencia;
-    }
-
-    public Conductor(String numeroLicencia, String categoriaLicencia, String id, String nombre, String apellido) {
-        super(id, nombre, apellido);
-        this.numeroLicencia = numeroLicencia;
-        this.categoriaLicencia = categoriaLicencia;
-    }
-
-    public String getNumeroLicencia() {
-        return numeroLicencia;
-    }
-
-    public void setNumeroLicencia(String numeroLicencia) {
-        this.numeroLicencia = numeroLicencia;
-    }
-
-    public String getCategoriaLicencia() {
-        return categoriaLicencia;
-    }
-
-    public void setCategoriaLicencia(String categoriaLicencia) {
-        this.categoriaLicencia = categoriaLicencia;
-    }
-
-    public void Imprimible() {
-        System.out.println("----- CONDUCTOR -----");
-        System.out.println("Cedula: " + id);
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Apellido: " + apellido);
-        System.out.println("Numero Licencia: " + numeroLicencia);
-        System.out.println("Categoria Licencia: " + categoriaLicencia);
-    }
-    
     
 }

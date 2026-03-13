d416274276a877371f37ed623d6283a4b4e1ed4c
Felix Herazo <fdherazo@unicesar.edu.co>
2026-03-12 22:30:42 -0500

    fix(model): correccion del constructor y metodo imprimirDetalle

--- SistemaTicketsTransCesar/src/Model/PasajeroRegular.java
+++ SistemaTicketsTransCesar/src/Model/PasajeroRegular.java
@@ -1,24 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
+package Model;
 
 public class PasajeroRegular extends Pasajero {
 
-    public PasajeroRegular(String cedula, String nombre, String apellido) {
-        super(cedula, nombre, apellido);
+    public PasajeroRegular(String cedula, String nombre) {
+        super(cedula, nombre);
     }
 
+    @Override
     public double calcularDescuento() {
         return 0.0;
     }
 
-    public void Imprimible() {
-        System.out.println("----- PASAJERO REGULAR -----");
-        System.out.println("Cedula: " + cedula);
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Apelido: " + apellido);
-        System.out.println("Descuento: 0%");
+    
+
+    @Override
+    public void imprimirDetalle() {
+        System.out.println("========== PASAJERO ==========");
+        System.out.println("Cédula    : " + cedula);
+        System.out.println("Nombre    : " + nombre);
+        System.out.println("Tipo      : Regular");
+        System.out.println("Descuento : 0%");
+        System.out.println("==============================");
     }
 }

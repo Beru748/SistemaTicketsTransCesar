d37d5065844a480a19182db01e1799687e257a5b
Felix Herazo <fdherazo@unicesar.edu.co>
2026-03-13 22:39:54 -0500

    feat(DAO): implementar metodo cargarTodos para leer tickets desde archivo

--- SistemaTicketsTransCesar/src/DAO/TicketDAO.java
+++ SistemaTicketsTransCesar/src/DAO/TicketDAO.java
@@ -1,9 +1,13 @@
 package DAO;
 
+import Model.Pasajero;
 import Model.Ticket;
+import Model.Vehiculo;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TicketDAO {
     private static final String RUTA_ARCHIVO = "tickets.txt";
@@ -14,5 +18,11 @@ public class TicketDAO {
         } catch (IOException e) {
             System.out.println("[ERROR] No se pudo guardar el ticket: " + e.getMessage());
         }
+    }
+    
+    public List<Ticket> cargarTodos(List<Pasajero> pasajeros, List<Vehiculo> vehiculos) {
+        List<Ticket> lista = new ArrayList<>();
+        return null;
+        
     }
 }

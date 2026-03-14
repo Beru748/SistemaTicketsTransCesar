68851769ba516d58392eb390218cc8d5d814b613
Beru748 <ericksamirwaton@gmail.com>
2026-03-14 10:40:42 -0500

    refactor(Model): ya con las nuevas funcionalidades se actualizan las clases

--- SistemaTicketsTransCesar/src/Model/Buseta.java
+++ SistemaTicketsTransCesar/src/Model/Buseta.java
@@ -2,8 +2,8 @@ package Model;
 
 public class Buseta extends Vehiculo{
     
-    public Buseta(String placa, String modelo, boolean estado, String idConductor, String origen, String destino) {
-    super(placa, modelo, 20, 4500.0, "BUSETA", estado, idConductor, origen, destino);
+    public Buseta(String placa, String modelo, boolean estado, String idConductor, Ruta rutaAsignada) {
+    super(placa, modelo, 20, 4500.0, "BUSETA", estado, idConductor, rutaAsignada);
 }
 
     @Override

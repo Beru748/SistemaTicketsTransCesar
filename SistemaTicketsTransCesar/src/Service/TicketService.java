2637400576c0cab92edc0b9854c0eb8e8646339d
Felix Herazo <fdherazo@unicesar.edu.co>
2026-03-13 23:12:42 -0500

    feat(Service): cargar tickets desde archivo al iniciar el servicio

--- SistemaTicketsTransCesar/src/Service/TicketService.java
+++ SistemaTicketsTransCesar/src/Service/TicketService.java
@@ -1,10 +1,19 @@
 package Service;
 
 import DAO.TicketDao;
+import Model.Pasajero;
 import Model.Ticket;
+import Model.Vehiculo;
 import java.util.List;
 
 public class TicketService {
     private final TicketDao ticketDAO;
     private List<Ticket> tickets;
+    
+    public TicketService(List<Pasajero> pasajeros, List<Vehiculo> vehiculos) {
+        this.ticketDAO = new TicketDao();
+        // Carga automática al iniciar: sesiones anteriores disponibles desde el primer momento
+        this.tickets = ticketDAO.cargarTodos(pasajeros, vehiculos);
+    }
+
 }

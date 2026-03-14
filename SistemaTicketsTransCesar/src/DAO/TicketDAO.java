21dc278ec339d7502a1eb770305fcf71448291d1
Felix Herazo <fdherazo@unicesar.edu.co>
2026-03-13 22:34:32 -0500

    feat(DAO): manejo de persistencia en clase Ticket

--- SistemaTicketsTransCesar/src/DAO/TicketDAO.java
+++ SistemaTicketsTransCesar/src/DAO/TicketDAO.java
@@ -1,12 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package DAO;
 
-/**
- *
- * @author COMPUMAX
- */
 public class TicketDAO {
-    
+    private static final String RUTA_ARCHIVO = "tickets.txt";
 }
